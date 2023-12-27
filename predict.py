eb7b83e3bcae68ef861eeffad44506fa298a6a49
td0630 <halitfurkan.turk@tomdigital.com.tr>
2023-12-28 01:26:13 +0300

    first commit

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import mlflow
-logged_model = 'runs:/57511fc38ba849cea62f6b67a7b53143/model'
+#logged_model = 'runs:/57511fc38ba849cea62f6b67a7b53143/model'
 logged_model='/Users/halitfurkanturk/Documents/zpersonal_repo/MLflow/mlruns/949594437793911251/57511fc38ba849cea62f6b67a7b53143/artifacts/model'
 
 # Load model as a PyFuncModel.
@@ -7,6 +7,7 @@ loaded_model = mlflow.pyfunc.load_model(logged_model)
 
 # Predict on a Pandas DataFrame.
 import pandas as pd
+import numpy as np
 data=pd.read_csv('pred_data.csv')
 predictions =loaded_model.predict(pd.DataFrame(data))
 print(predictions)
